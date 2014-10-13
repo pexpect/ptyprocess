eb81fe1d87e7d01aca5c803e92070a4f488f1f6b
Thomas Kluyver <takowl@gmail.com>
2014-10-13 12:28:14 -0700

    Bump version number to 0.3.1

--- ptyprocess/__init__.py
+++ ptyprocess/__init__.py
@@ -1,3 +1,3 @@
 from .ptyprocess import PtyProcess, PtyProcessUnicode, PtyProcessError
 
-__version__ = '0.3'
+__version__ = '0.3.1'
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst') as f:
     readme = f.read()
 
 setup(name='ptyprocess',
-      version='0.3',
+      version='0.3.1',
       description="Run a subprocess in a pseudo terminal",
       long_description=readme,
       author='Thomas Kluyver',
