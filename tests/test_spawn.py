249c671779761e91f437ac3e19cae834a35933d1
Jeff Quast <contact@jeffquast.com>
2015-04-26 23:16:50 +0000

    Workaround freebsd AT&T shell in test
    There is something I am not able to understand about /bin/sh
    on freebsd: This is actually some kind of "second output of
    input echo" that occurs only with the AT&T-compatible /bin/sh
    on FreeBSD, and intermittently (a modified version of the test
    runs forever on OSX and Linux).
    
    Complete analysis is in issue #12,
    https://github.com/pexpect/ptyprocess/issues/12#issuecomment-96443761

--- tests/test_spawn.py
+++ tests/test_spawn.py
@@ -2,43 +2,55 @@ import os
 import time
 import unittest
 from ptyprocess import PtyProcess, PtyProcessUnicode
+from ptyprocess.ptyprocess import which
 
 class PtyTestCase(unittest.TestCase):
-    def test_spawn_sh(self):
-        env = os.environ.copy()
-        env['FOO'] = 'rebar'
-        p = PtyProcess.spawn(['sh'], env=env)
-        p.read()
-        p.write(b'echo $FOO\n')
-        time.sleep(0.1)
-        response = p.read()
-        assert b'rebar' in response
-        
-        p.sendeof()
-        p.readline()
-        
-        with self.assertRaises(EOFError):
-            p.read()
+    def setUp(self):
+        self.cmd = u'echo $ENV_KEY; exit 0\n'
+        self.env = os.environ.copy()
+        self.env_key = u'ENV_KEY'
+        self.env_value = u'env_value'
+        self.env[self.env_key] = self.env_value
 
-    def test_spawn_unicode_sh(self):
-        env = os.environ.copy()
-        env['FOO'] = 'rebar'
-        p = PtyProcessUnicode.spawn(['sh'], env=env)
-        p.read()
-        p.write(u'echo $FOO\n')
-        time.sleep(0.1)
-        response = p.read()
-        assert u'rebar' in response
-        
-        p.sendeof()
-        p.readline()
-        
+    def _spawn_sh(self, cmd, outp):
+        # given,
+        p = PtyProcess.spawn(['sh'], env=self.env)
+        p.write(cmd)
+
+        # exercise,
+        while True:
+            try:
+                outp += p.read()
+            except EOFError:
+                break
+
+        # verify, read after EOF keeps throwing EOF
         with self.assertRaises(EOFError):
             p.read()
+        with self.assertRaises(EOFError):
+            p.readline()
+
+        # verify, input is echo to output
+        assert self.cmd.strip() in outp
+
+        # result of echo $ENV_KEY in output
+        assert self.env_value in outp
+
+        # exit succesfully (exit 0)
+        assert p.wait() == 0
+
+
+    def test_spawn_sh(self):
+        outp = b''
+        self._spawn_sh(self.cmd.encode('ascii'), outp)
+
+    def test_spawn_sh_unicode(self):
+        outp = u''
+        self._spawn_sh(self.cmd, outp)
 
     def test_quick_spawn(self):
         """Spawn a very short-lived process."""
         # so far only reproducable on Solaris 11, spawning a process
         # that exits very quickly raised an exception at 'inst.setwinsize',
         # because the pty file descriptor was quickly lost after exec().
-        PtyProcess.spawn(['/bin/true'])
+        PtyProcess.spawn(['true'])
