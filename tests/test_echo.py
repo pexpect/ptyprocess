63de9ec3de6f5dad9378c0ecb68cc06c19608988
Jeff Quast <contact@jeffquast.com>
2015-04-27 01:33:45 +0000

    simplify waitnoecho timeout. travis-ci freezes
    suspect read() return values are different on travis-ci
    than on teamcity.  Travis-CI seems to freeze up forever,
    whereas it passes fine on Linux TeamCity.

--- tests/test_echo.py
+++ tests/test_echo.py
@@ -24,33 +24,10 @@ class PtyEchoTestCase(unittest.TestCase):
 
     def test_waitnoecho_timeout(self):
         """Ensure waitnoecho() with timeout will return when using stty to unset echo."""
-        att_sh = PtyProcess.spawn(['sh'], echo=True)
-        # make a prompt we can expect,
-        time.sleep(1)
-        assert att_sh.getecho() == True
-        assert att_sh.waitnoecho(timeout=1) == False
-        att_sh.write(b'export PS1="IN: "\n')
-
-        # we must exhaust all awaiting input.  The terminal attributes made by
-        # setecho() are not understood by getecho() until all awaiting
-        # output is read, irregardless of the TCSANOW attribute we use, it is
-        # not reflected until this is done.
-        #
-        # Furthermore, with stdout line-buffered, we can expect .read() to
-        # return full lines.
-        inp = b''
-        while not inp == b'IN: ':
-             inp = att_sh.read().strip(b'\r\n')
-
-        # we use stty(1) to set echo. By doing so, we can be assured that
-        # waitnoecho() will return True even after a short duration (and after
-        # all of our output has been read.)
-        att_sh.write(b'stty echo\n')
-        while not inp == b'IN: ':
-             inp = att_sh.read().strip('\r\n')
-        assert att_sh.waitnoecho(timeout=3) == False
-        assert att_sh.getecho() == False
-
-        att_sh.write(b'exit 0\n')
-        self._read_until_eof(att_sh)
-        assert att_sh.wait() == 0
+        cat = PtyProcess.spawn(['cat'], echo=True)
+        assert cat.waitnoecho(timeout=1) == False
+        assert cat.echo == True
+        assert cat.getecho() == True
+        cat.sendeof()
+        self._read_until_eof(cat)
+        assert cat.wait() == 0
